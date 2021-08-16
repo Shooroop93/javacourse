9ff99ec2824acbf167bb288be4299359630b1fdd
Sergeev Roman <roman.sergeev1993@gmail.com>
2021-08-16 13:14:20 +0300

    add 9 day

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -1,7 +1,23 @@
 package day9.Task1;
 
+import day9.Task1.classes.Student;
+import day9.Task1.classes.Teacher;
+
 public class Task1 {
     public static void main(String[] args) {
+
+        Student student = new Student("Сергеев", "100");
+
+        Teacher teacher = new Teacher("Иванов", "Информатика");
+
+        student.printInfo();
+
+        teacher.printInfo();
+
+        System.out.println(student.getGroupName());
+        System.out.println(teacher.getSubjectName());
+        System.out.println(student.getName());
+        System.out.println(teacher.getName());
 
     }
 }
---
+++ src/main/java/day9/Task2/classes/Figure.java
@@ -0,0 +1,28 @@
+package day9.Task2.classes;
+
+public abstract class Figure {
+
+    private String color;
+
+    public Figure(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+
+
+
+
+
+
+}
